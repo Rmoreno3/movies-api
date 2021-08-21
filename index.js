f6adebc58a096440cfb06b1c908d1f08c729bc0c
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-08-21 00:47:42 -0400

    Implementando Boom para manejar los errores, creo el middlewares para el error 404 not found

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, handleError } = require('./utils/middlewares/errorHandlers');
+const {
+  logErrors,
+  handleError,
+  wrapErrors,
+} = require('./utils/middlewares/errorHandlers');
+
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 //body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
 
+// catch error 404 not found
+app.use(notFoundHandler);
+
+// Errors Middlewares
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(handleError);
 
 app.listen(config.port, () => {
