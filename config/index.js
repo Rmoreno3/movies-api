6cc18ddd4f81ef4adff69d26b781e142fb7fe482
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-08-26 20:48:15 -0400

    Agrego los scripts para generar los usuarios random, peliculas y los APItokens

--- config/index.js
+++ config/index.js
@@ -8,6 +8,11 @@ const config = {
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
+  // authJwtSecret: process.env.AUTH_JWT_SECRET,
 };
 
 module.exports = { config };
